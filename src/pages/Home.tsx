1631a5dcc9f1291e99930cb2cce34ec4c760bf5a
Anderson Hiroto <hiroto.anderson@gmail.com>
2021-06-21 23:57:24 -0300

    Add Firebase Auth
    Firebase Auth function added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import { useHistory } from 'react-router-dom';
+import { auth, firebase } from '../services/firebase';
+
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -10,8 +12,14 @@ import { Button } from '../components/Button';
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
+  function handleCreatRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+      history.push('/rooms/new');
+    })
+    
   }
 
   return (
@@ -24,7 +32,7 @@ export function Home() {
       <main>
         <div className="main-content">
         <img src={logoImg} alt="Letmeask" />
-        <button onClick={navigateToNewRoom} className="create-room">
+        <button onClick={handleCreatRoom} className="create-room">
           <img src={googleImg} alt="Logo do Google" />
           Crie uma sala com o Google
         </button>
