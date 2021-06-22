72a57f53cfb582475002d33070765c75d89a93d7
Anderson Hiroto <hiroto.anderson@gmail.com>
2021-06-22 02:18:25 -0300

    Editor config
    add eslint config 
    add prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -6,10 +7,12 @@ import { NewRoom } from "./pages/NewRoom";
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
-  )
-};
+  );
+}
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,49 @@
-import { useHistory } from 'react-router-dom';
-import { auth, firebase } from '../services/firebase';
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleImg from "../assets/images/google-icon.svg";
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleImg from '../assets/images/google-icon.svg';
-
-import '../styles/auth.scss';
-import { Button } from '../components/Button';
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function handleCreatRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+  async function handleCreatRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
 
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-      history.push('/rooms/new');
-    })
-    
+    history.push("/rooms/new");
   }
 
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
         <div className="main-content">
-        <img src={logoImg} alt="Letmeask" />
-        <button onClick={handleCreatRoom} className="create-room">
-          <img src={googleImg} alt="Logo do Google" />
-          Crie uma sala com o Google
-        </button>
-        <div className="separator">ou entre em uma sala</div>
-        <form>
-          <input 
-            type="text"
-            placeholder="Digite o código da sala"
-          />
-          <Button type="submit">
-            Entrar na sala
-          </Button>
-        </form>
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreatRoom} className="create-room">
+            <img src={googleImg} alt="Logo do Google" />
+            Crie uma sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
       </main>
     </div>
-  )
+  );
 }
